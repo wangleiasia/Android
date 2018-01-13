5911b6df8005bef18fad20356edafee989b68455
wangleiasia <517424651@qq.com>
2018-01-13 11:53:05 +0800

    去掉不需要的引用

--- first/src/com/example/first/MainActivity.java
+++ first/src/com/example/first/MainActivity.java
@@ -10,7 +10,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
